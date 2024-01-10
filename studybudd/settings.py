fc29b4669e8102862544be8f10f3e2de8d91cf0c
abhinav <abhinav70291@gmail.com>
2024-01-10 11:21:34 +0530

    added config changes

--- studybudd/settings.py
+++ studybudd/settings.py
@@ -24,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5-o$hv7$mw5bpsj-a##h6f@d1ylf8dq20rp-yuwn@7ct$plv=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'studybudd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'AcDBC53fAC4f-2F2c6g*ba3-E1g*b5Bc',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '37479',
     }
 }
 
@@ -126,7 +131,8 @@ AUTH_USER_MODEL='base.User'
 # STATIC_ROOT=
 MEDIA_URL="/images/"
 MEDIA_ROOT=BASE_DIR / "static/images"
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
