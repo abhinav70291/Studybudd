6bc8feb08bd9cd763826f50dad76bc4205b2e4c8
abhinav <abhinav70291@gmail.com>
2024-01-11 09:51:08 +0530

    host added

--- studybudd/settings_rail.py
+++ studybudd/settings_rail.py
@@ -3,6 +3,4 @@ from studybudd.settings import *
 from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
-EMAIL_HOST = config('EMAIL_HOST', default='localhost')
-EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+ALLOWED_HOSTS=["web-production-8fa9.up.railway.app/"]
